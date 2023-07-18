306c66ba8b762776134baa6785c1a5d10f5e1283
Patrice Cadiot <pathibul.r@gmail.com>
2023-07-18 23:04:33 +0200

    Update active_job_broadcaster.rb
    Another try to adapt ruby 3

--- lib/wisper/active_job_broadcaster.rb
+++ lib/wisper/active_job_broadcaster.rb
@@ -11,9 +11,9 @@ module Wisper
     class Wrapper < ::ActiveJob::Base
       queue_as :default
 
-      def perform(class_name, event, args)
+      def perform(class_name, event, args, kwargs)
         listener = class_name.constantize
-        listener.public_send(event, *args)
+        listener.public_send(event, *args, **kwargs)
       end
     end
 
